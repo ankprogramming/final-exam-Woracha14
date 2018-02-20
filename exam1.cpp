649f098c0576436087895dab5f75af5eaf114b4a
Unknown <milk756@hotmail.com>
2018-02-20 13:03:18 +0700

    ทำข้อที่ 1 เสร็จแล้ว

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    cout << "Welcome to CLI Calculator V1.00 " << endl;
+
+    float a, b, Result, cmd;
+    char n;
+    do {
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+
+    cout << "Menu" << endl;
+    cout << "1. +" << endl;
+    cout << "2. -" << endl;
+    cout << "3. *" << endl;
+    cout << "4. /" << endl;
+    cout << "5. quit" << endl;
+    cout << "Please Enter Number: ";
+    cin >> cmd;
+
+    if (cmd == 1) {
+        Result = a + b;
+        cout << Result << endl << endl;
+    } else if (cmd == 2) {
+        Result = a - b;
+        cout << Result << endl << endl;
+    } else if (cmd == 3) {
+        Result = a * b;
+        cout << Result << endl << endl;
+    } else if (cmd == 4) {
+        Result = a / b;
+        cout << Result << endl << endl;
+    } else {
+        break;
+    }
+
+    } while(1);
+
+
 
         return 0;
 }
